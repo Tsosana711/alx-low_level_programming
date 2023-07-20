c3997745a00c4f890e45f2b9f4d98074d77e8cfc
Tsosana711 <khanyo.tsosana@gmail.com>
2023-07-20 06:27:04 -0700

    TASK 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,10 @@
 
 void print_alphabet(void)
 {
-	for (char = 'a', char <= 'z', char++)
-	putchar("%c" / n, char);
-	return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
