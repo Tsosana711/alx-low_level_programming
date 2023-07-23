0d528b888d19a4000a7b47e84398fc84d80b5abd
Tsosana711 <khanyo.tsosana@gmail.com>
2023-07-23 15:40:55 -0700

    TASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/**
+ * _abs - computes the absolute value of an integer
+ *@k: is the integer
+ *Return: absolute value
+ */
+
+int _abs(int k)
+{
+int vee;
+if (k < 0)
+{
+vee = -k;
+}
+else
+{
+vee = k;
+}
+return (vee);
+}
